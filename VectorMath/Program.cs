9556cc936d9202e8b3f560a13ff5d31445f30ceb
Nathan Brown <njbrown@ksu.edu>
2019-12-09 14:14:40 -0600

    Operartor overloading

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,32 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+
+            Vector3 one = new Vector3(5, 4, 3);
+            Vector3 two = new Vector3(2, 8, 6);
+
+            Console.WriteLine("Adding two vectors: ");
+            Console.WriteLine($"With a static method: {Vector3.Add(one, two)}");
+
+            
+
+            Console.WriteLine($"With operator overloading: {one + two}");
+
+            Console.WriteLine($"Scaling: {one * 10}");
+
+            Console.WriteLine($"Vector comparison: {one == two}");
+
+            Console.WriteLine($"Vector comparison: {one == new Vector3(5,4,3)}");
+
+
+            if (one)
+            {
+                Console.WriteLine("Vector one is not zero");
+            }
+            if (two)
+            {
+                Console.WriteLine("Vector two is not zero");
+            }
         }
     }
 }
